48ed0acb80e32e3575d4f6670a4f63245ad53aa3
Chahrazad650 <Dahmanichahrazad4@gmail.com>
2026-03-14 03:24:42 +0100

    add .gitignore

--- display.py
+++
@@ -1,7 +0,0 @@
-def afficher_carte(terrain):
-    print("\nCarte de la mission :\n")
-    for ligne in terrain:
-        print("|", end=" ")
-        for cellule in ligne:
-            print(f"{cellule:^3}", end=" ")
-        print("|")
